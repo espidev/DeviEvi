53913e6e33f444370965051215d55bdd7bb64e98
espidev <aguy867@gmail.com>
2017-06-09 12:31:59 -0400

    Changes

--- main.js
+++ main.js
@@ -1,8 +1,13 @@
 const express = require('express');
 const app = express();
 
+app.use(express.static('views'));
+app.use(function(req, res, next){
+    console.log('[ERROR] Client error 404');
+    res.sendFile(__dirname + "/views/404.html");
+});
 app.get('/', function (req, res) {
-    res.sendFile(path.join(__dirname + "/index.html"));
+    res.sendFile(path.join(_dirname + "views/index.html"));
 });
 app.get('/admin', function (req, res) {
 
@@ -11,3 +16,10 @@ app.get('/admin', function (req, res) {
 app.listen(80, function () {
     console.log('Listening on port 80!')
 });
+
+function addMusic(){
+
+}
+function removeMusic(){
+
+}
