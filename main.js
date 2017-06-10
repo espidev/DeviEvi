2de5105986dc70de7499aa98f0a85eb5b28e8006
espidev <aguy867@gmail.com>
2017-06-10 14:40:52 -0400

    Changes

--- main.js
+++ main.js
@@ -1,5 +1,15 @@
 const express = require('express');
 const app = express();
+var server = require('http').createServer(app);
+var io = require('socket.io')(server);
+
+var muzik = [{"position":"1", "file":__dirname + "data/EspiDev - Serene.flac"}];
+var sockets = [];
+
+var fs = require('fs');
+if(!fs.existsSync(__dirname + "/data")){
+    fs.mkdir(__dirname + "/data");
+}
 
 app.use(express.static('views'));
 app.use(function(req, res, next){
@@ -10,7 +20,16 @@ app.get('/', function (req, res) {
     res.sendFile(path.join(_dirname + "views/index.html"));
 });
 app.get('/admin', function (req, res) {
+    res.sendFile(path.join(_dirname + "views/admin.html"));
+});
 
+io.on('connection', function(socket){
+    console.log('[INFO] New Socket.io client connection.');
+    socket.emit('data', createList());
+    sockets[socket.id] = socket;
+});
+io.on('disconnect', function(socket){
+    delete sockets[socket.id];
 });
 
 app.listen(80, function () {
@@ -18,7 +37,15 @@ app.listen(80, function () {
 });
 
 function createList(){
-
+    var str = "";
+    muzik.forEach(function(data){
+        var mm = require('musicmetadata');
+        var parser = mm(fs.createReadStream(data.file), function (err, metadata) {
+            if (err) throw err;
+            str += data.position + ":" + metadata.artist + ":" + metadata.album + ":" + metadata.albumartist + ":" + metadata.title + ":" + metadata.genre + ":" + metadata.picture + ":" + metadata.duration + " ";
+        });
+    });
+    return str;
 }
 function addMusic(){
 
